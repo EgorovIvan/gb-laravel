53eb52959b86cc3c9237e1fde3cd549513c04083
IvanE <sportsmenq@mail.ru>
2023-07-15 12:46:13 +0500

    add lesson 8: добавить регистрацию обычных пользователей, реализовать редактирование профилей(реализовал не полностью)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,7 @@ namespace App\Providers;
 use App\Queries\CategoriesQueryBuilder;
 use App\Queries\DataSourcesQueryBuilder;
 use App\Queries\NewsQueryBuilder;
+use App\Queries\ProfilesQueryBuilder;
 use App\Queries\QueryBuilder;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
@@ -21,6 +22,7 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(QueryBuilder::class, CategoriesQueryBuilder::class);
         $this->app->bind(QueryBuilder::class, NewsQueryBuilder::class);
         $this->app->bind(QueryBuilder::class, DataSourcesQueryBuilder::class);
+        $this->app->bind(QueryBuilder::class, ProfilesQueryBuilder::class);
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,28 +1,42 @@
 <?php
 
+use App\Http\Controllers\AccountController;
 use App\Http\Controllers\Admin\IndexController as AdminController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\NewsController;
 use App\Http\Controllers\Admin\NewsController as AdminNewsController;
 use App\Http\Controllers\Admin\CategoryController as AdminCategoryController;
 use App\Http\Controllers\Admin\DataSourceController as AdminDataSourceController;
+use App\Http\Controllers\Admin\ProfileController as AdminProfileController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [HomeController::class, 'index'])
     ->name('main.index');
 
-// Admin
-Route::group(['prefix' => 'admin', 'as' => 'admin.'], static function() {
-    Route::get('/', AdminController::class)
-        ->name('index');
-    Route::resource('/categories', AdminCategoryController::class);
-    Route::resource('/news', AdminNewsController::class);
-    Route::resource('/data-sources', AdminDataSourceController::class);
+Route::group(['middleware' => 'auth'], static function () {
+    Route::group(['prefix' => 'account'], static function () {
+        Route::get('/', AccountController::class)->name('account');
+    });
+
+    // Admin
+    Route::group([
+        'prefix' => 'admin',
+        'as' => 'admin.',
+        'middleware' => 'check.admin',
+
+    ], static function () {
+        Route::get('/', AdminController::class)
+            ->name('index');
+        Route::resource('/categories', AdminCategoryController::class);
+        Route::resource('/news', AdminNewsController::class);
+        Route::resource('/data-sources', AdminDataSourceController::class);
+        Route::resource('/users', AdminProfileController::class);
+    });
 });
 
 // Guest's routes
-
 Route::get('/news', [NewsController::class, 'index'])
     ->name('news.index');
 Route::get('/news/{news}', [NewsController::class, 'show'])
@@ -38,4 +52,14 @@ Route::match(["POST", 'GET', 'PUT'], '/test', function(\Illuminate\Http\Request 
     return (int) $request->isMethod('GET');
 });
 
+Route::get('/sessions', function () {
+    session()->put('mysession', 'Test session');
+    if (session()->has('mysession')) {
+        dd(session()->all(), session()->get('mysession'));
+        session()->forget('mysession');
+    }
+});
 
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
